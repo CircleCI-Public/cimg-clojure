5dfea9ceca4638eeb08bfef6abfc5f3bc8c14256
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-01-12 00:00:34 -0500

    Restart beta period for Clojure image [release] (#12)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.10/Dockerfile -t cimg/clojure:1.10.1  -t cimg/clojure:1.10 .
+docker build --file 1.10/Dockerfile -t cimg/clojure:1.10.3  -t cimg/clojure:1.10 .
+docker build --file 1.10/node/Dockerfile -t cimg/clojure:1.10.3-node  -t cimg/clojure:1.10-node .
+docker build --file 1.10/browsers/Dockerfile -t cimg/clojure:1.10.3-browsers  -t cimg/clojure:1.10-browsers .
---
+++ push-images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker push cimg/clojure:1.10.3
+docker push cimg/clojure:1.10
+docker push cimg/clojure:1.10.3-node
+docker push cimg/clojure:1.10-node
+docker push cimg/clojure:1.10.3-browsers
+docker push cimg/clojure:1.10-browsers

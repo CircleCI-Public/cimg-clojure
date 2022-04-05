8b86246c2c259cc7fbd799d18947adf79b1b5ec8
Jeff Chen <jeff.chen@circleci.com>
2022-04-05 16:37:41 -0700

    Publish v1.11.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.10/Dockerfile -t cimg/clojure:1.10.3  -t cimg/clojure:1.10 .
-docker build --file 1.10/node/Dockerfile -t cimg/clojure:1.10.3-node  -t cimg/clojure:1.10-node .
-docker build --file 1.10/browsers/Dockerfile -t cimg/clojure:1.10.3-browsers  -t cimg/clojure:1.10-browsers .
+docker build --file 1.11/Dockerfile -t cimg/clojure:1.11.1  -t cimg/clojure:1.11 .
+docker build --file 1.11/node/Dockerfile -t cimg/clojure:1.11.1-node  -t cimg/clojure:1.11-node .
+docker build --file 1.11/browsers/Dockerfile -t cimg/clojure:1.11.1-browsers  -t cimg/clojure:1.11-browsers .

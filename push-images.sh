47c892aebd8377f8e4bd4ba6b7700138d4930bdd
Jalexchen <jeff.chen@circleci.com>
2022-04-08 08:14:07 -0700

    Publish v1.11.1. [release] (#22)

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/clojure:1.10.3
-docker push cimg/clojure:1.10
-docker push cimg/clojure:1.10.3-node
-docker push cimg/clojure:1.10-node
-docker push cimg/clojure:1.10.3-browsers
-docker push cimg/clojure:1.10-browsers
+docker push cimg/clojure:1.11.1
+docker push cimg/clojure:1.11
+docker push cimg/clojure:1.11.1-node
+docker push cimg/clojure:1.11-node
+docker push cimg/clojure:1.11.1-browsers
+docker push cimg/clojure:1.11-browsers

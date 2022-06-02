aca87fd18a47e97740d61e34f145746fffeaa520
Jalexchen <jeff.chen@circleci.com>
2022-06-02 11:29:55 -0700

    [release] respinning with parentTags (#29)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,50 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/clojure:1.11.1
-docker push cimg/clojure:1.11
-docker push cimg/clojure:1.11.1-node
-docker push cimg/clojure:1.11-node
-docker push cimg/clojure:1.11.1-browsers
-docker push cimg/clojure:1.11-browsers
+docker push cimg/clojure:1.10-openjdk-8.0
+docker push cimg/clojure:1.10.3-openjdk-8.0
+docker push cimg/clojure:1.10-openjdk-8.0-node
+docker push cimg/clojure:1.10.3-openjdk-8.0-node
+docker push cimg/clojure:1.10-openjdk-8.0-browsers
+docker push cimg/clojure:1.10.3-openjdk-8.0-browsers
+docker push cimg/clojure:1.10-openjdk-11.0
+docker push cimg/clojure:1.10.3-openjdk-11.0
+docker push cimg/clojure:1.10-openjdk-11.0-node
+docker push cimg/clojure:1.10.3-openjdk-11.0-node
+docker push cimg/clojure:1.10-openjdk-11.0-browsers
+docker push cimg/clojure:1.10.3-openjdk-11.0-browsers
+docker push cimg/clojure:1.10-openjdk-17.0
+docker push cimg/clojure:1.10.3-openjdk-17.0
+docker tag cimg/clojure:1.10.3-openjdk-17.0 cimg/clojure:1.10.3
+docker tag cimg/clojure:1.10-openjdk-17.0 cimg/clojure:1.10
+docker push cimg/clojure:1.10-openjdk-17.0-node
+docker push cimg/clojure:1.10.3-openjdk-17.0-node
+docker tag cimg/clojure:1.10.3-openjdk-17.0-node cimg/clojure:1.10.3-node
+docker tag cimg/clojure:1.10-openjdk-17.0-node cimg/clojure:1.10-node
+docker push cimg/clojure:1.10-openjdk-17.0-browsers
+docker push cimg/clojure:1.10.3-openjdk-17.0-browsers
+docker tag cimg/clojure:1.10.3-openjdk-17.0-browsers cimg/clojure:1.10.3-browsers
+docker tag cimg/clojure:1.10-openjdk-17.0-browsers cimg/clojure:1.10-browsers
+docker push cimg/clojure:1.11-openjdk-8.0
+docker push cimg/clojure:1.11.1-openjdk-8.0
+docker push cimg/clojure:1.11-openjdk-8.0-node
+docker push cimg/clojure:1.11.1-openjdk-8.0-node
+docker push cimg/clojure:1.11-openjdk-8.0-browsers
+docker push cimg/clojure:1.11.1-openjdk-8.0-browsers
+docker push cimg/clojure:1.11-openjdk-11.0
+docker push cimg/clojure:1.11.1-openjdk-11.0
+docker push cimg/clojure:1.11-openjdk-11.0-node
+docker push cimg/clojure:1.11.1-openjdk-11.0-node
+docker push cimg/clojure:1.11-openjdk-11.0-browsers
+docker push cimg/clojure:1.11.1-openjdk-11.0-browsers
+docker push cimg/clojure:1.11-openjdk-17.0
+docker push cimg/clojure:1.11.1-openjdk-17.0
+docker tag cimg/clojure:1.11.1-openjdk-17.0 cimg/clojure:1.11.1
+docker tag cimg/clojure:1.11-openjdk-17.0 cimg/clojure:1.11
+docker push cimg/clojure:1.11-openjdk-17.0-node
+docker push cimg/clojure:1.11.1-openjdk-17.0-node
+docker tag cimg/clojure:1.11.1-openjdk-17.0-node cimg/clojure:1.11.1-node
+docker tag cimg/clojure:1.11-openjdk-17.0-node cimg/clojure:1.11-node
+docker push cimg/clojure:1.11-openjdk-17.0-browsers
+docker push cimg/clojure:1.11.1-openjdk-17.0-browsers
+docker tag cimg/clojure:1.11.1-openjdk-17.0-browsers cimg/clojure:1.11.1-browsers
+docker tag cimg/clojure:1.11-openjdk-17.0-browsers cimg/clojure:1.11-browsers
